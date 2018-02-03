ac97a36f26b1b3f5ebdff21893f55f85e143f3b5
tanuja0504 <tanuja55@yahoo.com>
2018-02-03 12:11:12 -0500

    Committing from Feature_2

--- sample.java
+++ sample.java
@@ -2,7 +2,11 @@ public class sample{
 public static void main(String as[])
 {
 //Comments new
+<<<<<<< HEAD
 //Coments for Feature Branch
 //Comments for feature_2 branch
+=======
+//Comments in Feature_2
+>>>>>>> Committing from Feature_2
 System.out.println("Hello World");
 }}
